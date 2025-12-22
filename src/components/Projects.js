558e6ed6270320a9c5313de86fae0a3db8d2f52a
Angel Singh Shrestha <angelshrestha2024@gmail.com>
2025-12-21 22:01:14 -0600

    UI changes to spiderman theme and other improvements

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -53,20 +53,28 @@ const Projects = () => {
 
   return (
     <section id="projects" className="projects">
-      <h2>Projects</h2>
+      {/* Centered Section Title */}
+      <div className="projects-title">
+        <h2>PROJECTS</h2>
+      </div>
+
+      {/* Projects Grid */}
       <div className="projects-container">
         {projects.map((project, index) => (
           <div key={index} className="project-card">
             <div className="project-image">
               <img src={project.image} alt={project.title} />
             </div>
+
             <div className="project-content">
               <h3>{project.title}</h3>
               <p>{project.description}</p>
 
               <div className="project-technologies">
                 {project.technologies.map((tech, idx) => (
-                  <span key={idx} className="tech-tag">{tech}</span>
+                  <span key={idx} className="tech-tag">
+                    {tech}
+                  </span>
                 ))}
               </div>
 
